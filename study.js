e625533a8650d84d90468958cb3e24283bbfbad2
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 20:07:04 +0530

    done

--- study.js
+++ study.js
@@ -1,4 +1,14 @@
-const message = 'blue;red;green';
+const message = '   good morning   ';
 
-//  split splits on the delimiter and returns string array
-const parts = message.split(';');
+const ts = message.trimStart(); //  'good morning   '
+
+const te = message.trimEnd(); //  '   good morning'
+
+const tr = message.trim(); //  'good morning'
+
+//  padding was introduced in ES2017
+//  maxLength should be greater than the length of the string
+
+const ps = tr.padStart(15, '#'); //  '###good morning'
+
+const pe = tr.padEnd(15, '%'); //  'good morning%%%'
