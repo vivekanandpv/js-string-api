1953415224177ccdf592bffdfe07d0f7f99a36f2
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 20:12:49 +0530

    done

--- study.js
+++ study.js
@@ -7,3 +7,7 @@ const ew = message.endsWith('ing'); //  true
 const i = message.indexOf('o'); //  1; first index
 
 const e = message.lastIndexOf('o'); //  6
+
+const index = message.search('oo'); //  1
+
+const result = message.includes('oo'); //  true
