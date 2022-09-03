4cddcc9140e1d5baadb8dadb85bc57627bcc3030
Vivekanand P V <vivekanandpv@outlook.com>
2022-09-03 19:45:22 +0530

    done

--- study.js
+++ study.js
@@ -1,16 +1,5 @@
-//  strings are primitives and immutable in JavaScript
-//  single quotes or double quotes or backticks can be used to mark strings
-//  single quotes are usually favoured
+const message = 'Good Morning';
 
-const message = 'Greetings of the day!';
+const uc = message.toUpperCase();
 
-console.log(typeof message); //  string
-
-//  backticks are used to create template literals.
-//  Use ${} to insert values in the template
-const year = 2022;
-const city = 'Bengaluru';
-
-const line = `This is ${city} in the year ${year}`;
-
-console.log(line);
+const lc = message.toLowerCase();
